56642e50e64075f1aae721626d251bf90caea335
ZhouYang <pointer19910926@gmail.com>
2023-03-23 18:06:19 +0800

    123123

--- src/pages/addresses/ERC20Transfers.tsx
+++ src/pages/addresses/ERC20Transfers.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { ERC20TransferVO } from "../../services";
 import { fetchAddressERC20Transfers } from "../../services/tx";
-import { Table, Typography, Row, Col , PaginationProps } from 'antd';
+import { Table, Typography, Row, Col, PaginationProps } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { useTranslation } from 'react-i18next';
 import AddressTag, { ShowStyle } from '../../components/AddressTag';
@@ -9,10 +9,11 @@ import TransactionHash from '../../components/TransactionHash';
 import { DateFormat } from '../../utils/DateUtil';
 import EtherAmount from '../../components/EtherAmount';
 
-const {  Text } = Typography;
+const { Text } = Typography;
 
 export default ({ address }: { address: string }) => {
     const { t } = useTranslation();
+
     async function doFetchAddressTransactions() {
         fetchAddressERC20Transfers({
             current: pagination.current,
@@ -28,14 +29,16 @@ export default ({ address }: { address: string }) => {
             setTableData(data.records);
         })
     }
+
+    function paginationOnChange(current: number, pageSize: number) {
+        pagination.current = current;
+        doFetchAddressTransactions();
+    }
     const [pagination, setPagination] = useState<PaginationProps>({
         current: 1,
         pageSize: 10,
         showTotal: (total) => <>Total : {total}</>,
-        onChange: (page, pageSize) => {
-            pagination.current = page;
-            doFetchAddressTransactions();
-        }
+        onChange:paginationOnChange
     });
     const [tableData, setTableData] = useState<ERC20TransferVO[]>([]);
 
@@ -65,16 +68,16 @@ export default ({ address }: { address: string }) => {
                 <Row>
                     <Col span={20}>
                         {
-                            address === val 
+                            address === val
                                 ? <AddressTag address={val} sub={8} showStyle={ShowStyle.NO_LINK} />
                                 : <AddressTag address={val} sub={8} />
                         }
                     </Col>
                     <Col span={4}>
                         {
-                            address === val 
-                                ? <Text code strong style={{color:"orange"}}>OUT</Text>
-                                : <Text code strong style={{color:"green"}}>IN</Text>
+                            address === val
+                                ? <Text code strong style={{ color: "orange" }}>OUT</Text>
+                                : <Text code strong style={{ color: "green" }}>IN</Text>
                         }
                     </Col>
                 </Row>
@@ -85,7 +88,7 @@ export default ({ address }: { address: string }) => {
             dataIndex: 'to',
             width: 180,
             render: (val) => <>{
-                address === val 
+                address === val
                     ? <AddressTag address={val} sub={8} showStyle={ShowStyle.NO_LINK} />
                     : <AddressTag address={val} sub={8} />
             }</>
@@ -101,7 +104,7 @@ export default ({ address }: { address: string }) => {
     return <>
 
         <Table columns={columns} dataSource={tableData} scroll={{ x: 800 }}
-            pagination={pagination} rowKey={ (vo) => vo.transactionHash }
+            pagination={pagination} rowKey={(vo) => vo.transactionHash}
         />
 
     </>
