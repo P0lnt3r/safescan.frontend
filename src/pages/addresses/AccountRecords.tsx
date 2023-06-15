4bac19b97f032b6e367efeb0e7d2f492c5205eec
ZhouYang <pointer19910926@gmail.com>
2023-06-16 01:09:02 +0800

    update accountrecord show ui

--- src/pages/addresses/AccountRecords.tsx
+++ src/pages/addresses/AccountRecords.tsx
@@ -1,8 +1,8 @@
 
 import { useCallback, useEffect, useMemo, useState } from "react"
-import { AccountRecordVO, NodeRewardVO, TransactionVO } from "../../services";
+import { AccountRecordVO, AddressPropVO, NodeRewardVO, TransactionVO } from "../../services";
 import { fetchAddressTransactions } from "../../services/tx";
-import { PaginationProps, Table, Typography, Row, Col, Tooltip } from 'antd';
+import { PaginationProps, Table, Typography, Row, Col, Tooltip, TableColumnsType, Divider } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { useTranslation } from 'react-i18next';
 import AddressTag, { ShowStyle } from '../../components/AddressTag';
@@ -19,6 +19,16 @@ import { fetchAddressAccountRecord } from "../../services/accountRecord";
 
 const { Text, Link } = Typography;
 const { Column, ColumnGroup } = Table;
+
+interface ExpandedAccountRecordDataType {
+    key: React.Key
+    transactionHash: string
+    action: string
+    nodeAddress: string,
+    nodeAddressPropVO: AddressPropVO
+    freezeHeight: number,
+    unfreezeHeight: number
+}
 
 export default ({ address }: { address: string }) => {
     const { t } = useTranslation();
@@ -56,69 +66,97 @@ export default ({ address }: { address: string }) => {
         doFetchAddressAccountRecords();
     }, [address]);
 
-    const RewardTypeLabel = (rewardType: number) => {
-        switch (rewardType) {
-            case 1:
-                return "Creator";
-            case 2:
-                return "Founder";
-            case 3:
-                return "Voter";
+    const expandedRowRender = (accountRecord: AccountRecordVO) => {
+        const {
+            sepcialAddress, nodeAddressPropVO, freezeHeight, unfreezeHeight,
+            votedAddress, votedAddressPropVO, voteHeight, releaseHeight
+        } = accountRecord;
+        const columns: TableColumnsType<ExpandedAccountRecordDataType> = [
+            { title: 'Action', dataIndex: 'action', key: 'action', width: 50 },
+            {
+                title: 'Transaction Hash', dataIndex: 'transactionHash', width: 20,
+                render: (transactionHash) => {
+                    return <div>
+                        <TransactionHash txhash={transactionHash} sub={5}></TransactionHash>
+                    </div>
+                },
+            },
+            {
+                title: "Node", dataIndex: "nodeAddress", width: 20,
+                render: (nodeAddress, expandedAccountRecord) => {
+                    const hasLink = !(nodeAddress == address);
+                    return <>
+                        <Tooltip title={nodeAddress}>
+                            {
+                                expandedAccountRecord.nodeAddressPropVO &&
+                                <>
+                                    {!hasLink && <>{accountRecord.nodeAddressPropVO.tag}</>}
+                                    {hasLink && <RouterLink to={`/address/${nodeAddress}`}>
+                                        <Link ellipsis>{accountRecord.nodeAddressPropVO.tag}</Link>
+                                    </RouterLink>}
+                                </>
+                            }
+                            {
+                                !expandedAccountRecord.nodeAddressPropVO &&
+                                <>
+                                    {!hasLink && <Text ellipsis>{nodeAddress}</Text>}
+                                    {hasLink && <RouterLink to={`/address/${nodeAddress}`}>
+                                        <Link ellipsis>{nodeAddress}</Link>
+                                    </RouterLink>}
+                                </>
+                            }
+                        </Tooltip>
+                    </>
+                },
+            },
+            {
+                title: 'Freeze', dataIndex: 'freezeHeight', render: (freezeHeight) => {
+                    return <>{freezeHeight} <Divider type="vertical" />2020-12-12 12:12:12</>
+                }
+            },
+            {
+                title: 'Unfreeze', dataIndex: 'unfreezeHeight', render: (unfreezeHeight) => {
+                    return <>{unfreezeHeight} <Divider type="vertical" />2020-12-12 12:12:12</>
+                }
+            },
+        ];
+        const data = [];
+        if (sepcialAddress) {
+            data.push({
+                key: "",
+                transactionHash: "0x0000000000000000000000000000000000000000",
+                action: "SNRegister",
+                nodeAddress: sepcialAddress,
+                nodeAddressPropVO,
+                freezeHeight,
+                unfreezeHeight
+            });
         }
-    }
-
-    const columns: ColumnsType<AccountRecordVO> = [
-        {
-            title: <Text strong style={{ color: "#6c757e" }}>Lock ID</Text>,
-            dataIndex: 'lockId',
-            width: 80,
-            render: lockId => <>{lockId}</>
-        },
-        {
-            title: <Text strong style={{ color: "#6c757e" }}>Amount</Text>,
-            dataIndex: 'amount',
-            width: 100,
-            render: (value) => <Text strong><EtherAmount raw={value} /></Text>
-        },
-        {
-            title: <Text strong style={{ color: "#6c757e" }}>Lock Day</Text>,
-            dataIndex: 'lockDay',
-            width: 130,
-            render: (lockDay) => <>{lockDay}</>
-        },
-        {
-            title: <Text strong style={{ color: "#6c757e" }}>Start-Height</Text>,
-            dataIndex: 'startHeight',
-            width: 180,
-            render: (startHeight, accountRecord) => {
-                return <>
-                    {startHeight}
-                </>
-            }
-        },
-        {
-            title: <Text strong style={{ color: "#6c757e" }}>Unlock-Height</Text>,
-            dataIndex: 'unlockHeight',
-            width: 180,
-            render: (unlockHeight, accountRecord) => {
-                return <>
-                    {unlockHeight}
-                </>
-            }
-        },
-    ];
+        if (votedAddress) {
+            data.push({
+                key: "",
+                transactionHash: "0x0000000000000000000000000000000000000000",
+                action: "Vote",
+                nodeAddress: votedAddress,
+                nodeAddressPropVO: votedAddressPropVO,
+                freezeHeight: voteHeight,
+                unfreezeHeight: releaseHeight
+            })
+        }
+        return <Table columns={columns} dataSource={data} pagination={false} />;
+    };
 
     return <>
 
         <Table dataSource={tableData} scroll={{ x: 800 }}
+            expandable={{ expandedRowRender }}
             pagination={pagination} rowKey={(accountRecord: AccountRecordVO) => accountRecord.lockId}>
-
-            <Column title={<Text strong style={{ color: "#6c757e" }}>Lock-ID</Text>}
+            <Column title={<Text strong style={{ color: "#6c757e" }}>ID</Text>}
                 dataIndex="lockId"
                 render={(lockId) => {
                     return <>{lockId}</>
                 }}
-                width={80}
+                width={20}
                 fixed
             />
             <Column title={<Text strong style={{ color: "#6c757e" }}>Amount</Text>}
@@ -126,128 +164,96 @@ export default ({ address }: { address: string }) => {
                 render={(amount) => {
                     return <Text strong><EtherAmount raw={amount} /></Text>
                 }}
-                width={150}
+                width={80}
+            />
+
+            <Column title={<Text strong style={{ color: "#6c757e" }}>Member Of Node</Text>}
+                dataIndex="sepcialAddress"
+                render={(sepcialAddress, accountRecord: AccountRecordVO) => {
+                    const hasLink = !(sepcialAddress == address);
+                    return <>
+                        <Tooltip title={sepcialAddress}>
+                            {
+                                accountRecord.nodeAddressPropVO &&
+                                <>
+                                    {!hasLink && <>{accountRecord.nodeAddressPropVO.tag}</>}
+                                    {hasLink && <RouterLink to={`address/${sepcialAddress}`}>
+                                        <Link ellipsis>{accountRecord.nodeAddressPropVO.tag}</Link>
+                                    </RouterLink>}
+                                </>
+                            }
+                            {
+                                !accountRecord.nodeAddressPropVO &&
+                                <>
+                                    {!hasLink && <Text ellipsis>{sepcialAddress}</Text>}
+                                    {hasLink && <RouterLink to={`address/${sepcialAddress}`}>
+                                        <Link ellipsis>{sepcialAddress}</Link>
+                                    </RouterLink>}
+                                </>
+                            }
+                        </Tooltip>
+                    </>
+                }}
+                width={80}
+            />
+            <Column title={<Text strong style={{ color: "#6c757e" }}>Proxy MasterNode</Text>}
+                dataIndex=""
+                render={() => {
+                    return <>{ }</>
+                }}
+                width={80}
+            />
+            <Column title={<Text strong style={{ color: "#6c757e" }}>Vote For Node</Text>}
+                dataIndex="votedAddress"
+                render={(votedAddress, accountRecord: AccountRecordVO) => {
+                    const hasLink = !(votedAddress == address);
+                    return <>
+                        <Tooltip title={votedAddress}>
+                            {
+                                accountRecord.votedAddressPropVO &&
+                                <>
+                                    {!hasLink && <>{accountRecord.nodeAddressPropVO.tag}</>}
+                                    {hasLink && <RouterLink to={`address/${votedAddress}`}>
+                                        <Link ellipsis>{accountRecord.votedAddressPropVO.tag}</Link>
+                                    </RouterLink>}
+                                </>
+                            }
+                            {
+                                !accountRecord.votedAddressPropVO &&
+                                <>
+                                    {!hasLink && <Text ellipsis>{votedAddress}</Text>}
+                                    {hasLink && <RouterLink to={`address/${votedAddress}`}>
+                                        <Link ellipsis>{votedAddress}</Link>
+                                    </RouterLink>}
+                                </>
+                            }
+                        </Tooltip>
+                    </>
+                }}
+                width={80}
+            />
+
+            <Column title={<Text strong style={{ color: "#6c757e" }}>Start</Text>}
+                dataIndex="startHeight"
+                render={(startHeight) => {
+                    return <>{startHeight} <Divider type="vertical" /> 2020-12-12 12:12:12</>
+                }}
+                width={100}
             />
             <Column title={<Text strong style={{ color: "#6c757e" }}>Lock Day</Text>}
                 dataIndex="lockDay"
                 render={(lockDay) => {
                     return <Text>{lockDay}</Text>
                 }}
-                width={80}
-            />
-            <Column title={<Text strong style={{ color: "#6c757e" }}>Start</Text>}
-                dataIndex="startHeight"
-                render={(startHeight) => {
-                    return <>{startHeight}</>
-                }}
-                width={80}
+                width={40}
             />
             <Column title={<Text strong style={{ color: "#6c757e" }}>Unlock</Text>}
                 dataIndex="unlockHeight"
                 render={(unlockHeight) => {
-                    return <>{unlockHeight}</>
+                    return <>{unlockHeight}<Divider type="vertical" /> 2020-12-12 12:12:12</>
                 }}
-                width={80}
+                width={100}
             />
-
-            <ColumnGroup title="节点竞选">
-                <Column title={<Text strong style={{ color: "#6c757e" }}>Node Address</Text>}
-                    dataIndex="sepcialAddress"
-                    render={(sepcialAddress, accountRecord: AccountRecordVO) => {
-                        const hasLink = !(sepcialAddress == address);
-                        return <>
-                            <Tooltip title={sepcialAddress}>
-                                {
-                                    accountRecord.nodeAddressPropVO &&
-                                    <>
-                                        {!hasLink && <>{accountRecord.nodeAddressPropVO.tag}</>}
-                                        {hasLink && <RouterLink to={`address/${sepcialAddress}`}>
-                                            <Link ellipsis>{accountRecord.nodeAddressPropVO.tag}</Link>
-                                        </RouterLink>}
-                                    </>
-                                }
-                                {
-                                    !accountRecord.nodeAddressPropVO &&
-                                    <>
-                                        {!hasLink && <Text ellipsis>{sepcialAddress}</Text>}
-                                        {hasLink && <RouterLink to={`address/${sepcialAddress}`}>
-                                            <Link ellipsis>{sepcialAddress}</Link>
-                                        </RouterLink>}
-                                    </>
-                                }
-                            </Tooltip>
-                        </>
-                    }}
-                    width={160}
-                />
-                <Column title={<Text strong style={{ color: "#6c757e" }}>Freeze</Text>}
-                    dataIndex="freezeHeight"
-                    render={(freezeHeight) => {
-                        return <>{freezeHeight}</>
-                    }}
-                    width={80}
-                />
-                <Column title={<Text strong style={{ color: "#6c757e" }}>Unfreeze</Text>}
-                    dataIndex="unfreezeHeight"
-                    render={(unfreezeHeight) => {
-                        return <>{unfreezeHeight}</>
-                    }}
-                    width={80}
-                />
-            </ColumnGroup>
-
-            <ColumnGroup title="节点投票">
-                <Column title={<Text strong style={{ color: "#6c757e" }}>代理节点</Text>}
-                    dataIndex=""
-                    render={() => {
-                        return <>{ }</>
-                    }}
-                    width={80}
-                />
-                <Column title={<Text strong style={{ color: "#6c757e" }}>Node Address</Text>}
-                    dataIndex="votedAddress"
-                    render={(votedAddress, accountRecord: AccountRecordVO) => {
-                        const hasLink = !(votedAddress == address);
-                        return <>
-                            <Tooltip title={votedAddress}>
-                                {
-                                    accountRecord.votedAddressPropVO &&
-                                    <>
-                                        {!hasLink && <>{accountRecord.votedAddressPropVO.tag}</>}
-                                        {hasLink && <RouterLink to={`address/${votedAddress}`}>
-                                            <Link ellipsis>{accountRecord.votedAddressPropVO.tag}</Link>
-                                        </RouterLink>}
-                                    </>
-                                }
-                                {
-                                    !accountRecord.votedAddressPropVO &&
-                                    <>
-                                        {!hasLink && <Text ellipsis>{votedAddress}</Text>}
-                                        {hasLink && <RouterLink to={`address/${votedAddress}`}>
-                                            <Link ellipsis>{votedAddress}</Link>
-                                        </RouterLink>}
-                                    </>
-                                }
-                            </Tooltip>
-                        </>
-                    }}
-                    width={160}
-                />
-                <Column title={<Text strong style={{ color: "#6c757e" }}>Freeze</Text>}
-                    dataIndex="voteHeight"
-                    render={(voteHeight) => {
-                        return <>{voteHeight}</>
-                    }}
-                    width={80}
-                />
-                <Column title={<Text strong style={{ color: "#6c757e" }}>Unfreeze</Text>}
-                    dataIndex="releaseHeight"
-                    render={(releaseHeight) => {
-                        return <>{releaseHeight}</>
-                    }}
-                    width={80}
-                />
-            </ColumnGroup>
 
         </Table>
 
